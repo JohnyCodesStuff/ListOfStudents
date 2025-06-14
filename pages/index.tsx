33fda1d150a21d748f6076313187f3656872ae00
JohnyCodesStuff <janpelczar12@gmail.com>
2025-06-14 18:41:43 +0200

    deleting learn tag from main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,10 +32,7 @@ const Home: NextPage = () => {
             <p>Find in-depth information about Next.js features and API.</p>
           </Link>
 
-          <Link href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </Link>
+          
 
           
         </div>
